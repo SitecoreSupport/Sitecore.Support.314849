3b3da7b9704b2f872335e561d734a5d87f9f5837
Paul Stupka <paul.stupka@sitecore.com>
2019-10-20 13:41:30 +0300

    empty project

---
+++ src/Sitecore.Support.314849/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.314849")]
+[assembly: AssemblyProduct("Sitecore.Support.314849")]
+[assembly: ComVisible(false)]
