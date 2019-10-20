a23bb25d47c6a99339d4254fe64efd940016fc5f
Paul Stupka <paul.stupka@sitecore.com>
2019-10-20 13:42:59 +0300

    fix the issue

--- src/Sitecore.Support.314849/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.314849/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.314849")]
 [assembly: AssemblyProduct("Sitecore.Support.314849")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Remove unused _url and _lastestversion virtual fields to improve performance of facet query requests")]
+
